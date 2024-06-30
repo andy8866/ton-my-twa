a6eb547bb275a91b825290d59efba8903da9ed79
Andy <Andy.8866@outlook.com>
2024-07-01 01:52:11 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://github.com/andy8866/ton-my-twa/public/tonconnect-manifest.json';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
